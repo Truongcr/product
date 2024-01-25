cbba85c55e11a908056a6cf8b497f446ddeaf635
Truongcr <truongcr1609@gmail.com>
2024-01-25 22:56:05 +0700

    update-1

--- src/main/java/com/tramhuong/product/ProductApplication.java
+++ src/main/java/com/tramhuong/product/ProductApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@EnableDiscoveryClient
 @ComponentScan(basePackages = "com.tramhuong")
 public class ProductApplication {
 
